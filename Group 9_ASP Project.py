a92a4b3dacd21d832b990a8eca727524c0c6f537
Cheryl <jisatsu70@gmail.com>
2021-08-19 13:48:21 +0800

    hello :)

--- Group 9_ASP Project.py
+++ Group 9_ASP Project.py
@@ -1,14 +1,10 @@
 import pandas as pd
-data = pd.read_excel('IMVA.xls',sheet_name="IMVA")
-print(data)
 
-files = data['Periods'].str.split(' ',n=1,expand=True)
-print(files)
+dataframe = None
+with pd.ExcelFile("IMVA.xls") as reader:
+    dataframe = pd.read_excel(reader, sheet_name='IMVA', usecols="A,AM:AZ")
+print(dataframe)
 
-data = data.assign(year=data[1])
 
-data.index = data['year']
-print(data.index)
 
-set1= data[(data['year'] >=str(2008)) & (data['year'] <= str(2017))]
-print(set1)
+
