adda0634bae4a270f6b8d7c5eb1b35fc782adf11
Ameera <nurameera1802@gmail.com>
2021-08-19 11:13:41 +0800

    hey

--- Group 9_ASP Project.py
+++ Group 9_ASP Project.py
@@ -1,3 +1,14 @@
 import pandas as pd
 file= pd.read_excel('IMVA.xls')
 print(file)
+
+files=file['Period'].str.split(' ',n=1,expand=True)
+print(files)
+
+file= file.assign(year=files[1])
+
+file.index=file['year']
+print(file.index)
+
+set1=file[(file['year'] >=str(2008)) & (file['year'] <= str(2017))]
+print(setC)
